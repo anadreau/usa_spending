76ca311bd39ff57d9288ee0eba6dbf9492a0384a
Alex Nadreau <anadreau42@gmail.com>
2023-06-13 10:23:53 -0700

    added documentation for StateOverview

--- lib/src/api/state_overview_model.dart
+++ lib/src/api/state_overview_model.dart
@@ -1,4 +1,6 @@
+///StateOverview class
 class StateOverview {
+  ///StateOverview Constructor
   StateOverview({
     required this.amount,
     required this.code,
@@ -7,18 +9,35 @@ class StateOverview {
     required this.type,
   });
 
+  ///Initialize Name [String]
   String name;
+
+  ///Initialize code [String]
   String code;
+
+  ///Initialize fips [String]
   String fips;
+
+  ///Initialize amount [double]
   double amount;
+
+  ///Initialize type [Type]
   Type type;
 }
 
+///Enum for State [Type]
 enum Type {
+  ///State
   state(name: 'State'),
+
+  ///Territory
   territory(name: 'Territory'),
+
+  ///District
   district(name: 'District');
 
   const Type({required this.name});
+
+  ///Initialize Name
   final String name;
 }
